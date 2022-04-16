367286968489ac566fb4856422c5aa3fc05dd6f2
Max Hu <max@12450.top>
2022-04-16 01:46:01 -0400

    finished

---
+++ modules/coin.mjs
@@ -0,0 +1,92 @@
+/** Coin flip functions 
+ * This module will emulate a coin flip given various conditions as parameters as defined below
+ */
+
+/** Simple coin flip
+ * 
+ * Write a function that accepts no parameters but returns either heads or tails at random.
+ * 
+ * @param {*}
+ * @returns {string} 
+ * 
+ * example: coinFlip()
+ * returns: heads
+ * 
+ */
+
+function coinFlip() {
+  if (Math.random() < 0.5) return 'heads';
+  else return 'tails';
+}
+
+/** Multiple coin flips
+ * 
+ * Write a function that accepts one parameter (number of flips) and returns an array of 
+ * resulting "heads" or "tails".
+ * 
+ * @param {number} flips 
+ * @returns {string[]} results
+ * 
+ * example: coinFlips(10)
+ * returns:
+ *  [
+      'heads', 'heads',
+      'heads', 'tails',
+      'heads', 'tails',
+      'tails', 'heads',
+      'tails', 'heads'
+    ]
+ */
+
+function coinFlips(flips) {
+  return Array(flips).fill(0).map((_, i) => coinFlip());
+}
+
+/** Count multiple flips
+ * 
+ * Write a function that accepts an array consisting of "heads" or "tails" 
+ * (e.g. the results of your `coinFlips()` function) and counts each, returning 
+ * an object containing the number of each.
+ * 
+ * example: countFlips(['heads', 'heads','heads', 'tails','heads', 'tails','tails', 'heads','tails', 'heads'])
+ * { tails: 5, heads: 5 }
+ * 
+ * @param {string[]} array 
+ * @returns {{ heads: number, tails: number }}
+ */
+
+function countFlips(array) {
+  return array.reduce((res, currentValue) => {
+    if (currentValue === 'heads') res.heads += 1;
+    else if (currentValue === 'tails') res.tails += 1;
+    else throw 'Wrong type of array elements!';
+    return res;
+  }, { tails: 0, heads: 0 });
+}
+
+/** Flip a coin!
+ * 
+ * Write a function that accepts one input parameter: a string either "heads" or "tails", flips a coin, and then records "win" or "lose". 
+ * 
+ * @param {string} call 
+ * @returns {object} with keys that are the input param (heads or tails), a flip (heads or tails), and the result (win or lose). See below example.
+ * 
+ * example: flipACoin('tails')
+ * returns: { call: 'tails', flip: 'heads', result: 'lose' }
+ */
+
+function flipACoin(call) {
+  const flip = coinFlip();
+  return {
+    call,
+    flip,
+    result: call === flip ? 'win' : 'lose'
+  };
+}
+
+
+/** Export 
+ * 
+ * Export all of your named functions
+*/
+export { coinFlip, coinFlips, countFlips, flipACoin };
